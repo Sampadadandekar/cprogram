bb4659498319d74ee808e830a48886f75fabf4f7
Sampadadandekar <144693852+Sampadadandekar@users.noreply.github.com>
2025-08-13 12:29:42 +0530

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int num1, num2, sum;
+printf("enter two no.: ");
+scanf("%d %d", &num1, &num2);
+sum= num1+num2 ;
+printf("Addition of %d and %d=%d\n", num1, num2,sum);
+return 0;
+}
