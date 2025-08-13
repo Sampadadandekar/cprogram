370a4321f3d265054a39e540361516eba3f70538
Sampadadandekar <144693852+Sampadadandekar@users.noreply.github.com>
2025-08-13 12:35:56 +0530

    Add files via upload

---
+++ Division.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int num1, num2,div ;
+printf("enter two no.: ");
+scanf("%d %d", &num1, &num2);
+ div= num1/num2 ;
+printf("Division of %d and %d=%d\n", num1, num2, div);
+return 0;
+}
