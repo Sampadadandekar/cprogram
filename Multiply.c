bb3edac52702a94f93adeb71640dfae3cbd098b8
Sampadadandekar <144693852+Sampadadandekar@users.noreply.github.com>
2025-08-13 12:32:59 +0530

    Add files via upload

---
+++ Multiply.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int num1, num2, multipy ;
+printf("enter two no.: ");
+scanf("%d %d", &num1, &num2);
+ multipy= num1*num2 ;
+printf("Multiplication of %d and %d=%d\n", num1, num2, multipy);
+return 0;
+}
