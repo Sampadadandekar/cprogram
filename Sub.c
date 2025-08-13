f6fc832dfcd13bfcefe426976ecb0decf1b2f316
Sampadadandekar <144693852+Sampadadandekar@users.noreply.github.com>
2025-08-13 12:22:24 +0530

    Add files via upload

---
+++ Sub.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int num1, num2, sub;
+printf("enter two no.: ");
+scanf("%d %d", &num1, &num2);
+sub= num1-num2 ;
+printf("Subtraction of %d and %d=%d\n", num1, num2,sub);
+return 0;
+}
